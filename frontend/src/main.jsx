f0b605934c9182b48ccb6ff6b68e8396eee980d8
lilbovio <lilbovio30@gmail.com>
2025-05-08 11:37:51 -0600

    YA JALOOO

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
-import Login from './pages/Login.jsx';
-import Home from './pages/Home.jsx';
-import BusinessReview from './pages/BusinessReview.jsx';
-
-import '../../frontend/index.css'; // Asegúrate de tener un archivo CSS para estilos globales
+import '../../frontend/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="review/:id" element={<BusinessReview />} />
-        </Route>
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
