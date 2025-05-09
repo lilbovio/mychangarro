f6e24f0f557c481e5af2a5c7f13c0fc450722bd1
lilbovio <lilbovio30@gmail.com>
2025-05-09 05:17:20 -0600

    fakiu ugo

---
+++ sentencias sql.sql
@@ -0,0 +1,49 @@
+-- Negocio 1 (Comida)
+INSERT INTO negocios (nombre, descripcion, direccion, categoria, imagen)
+VALUES (
+  'Tacos "El Güero"', 
+  'Los mejores tacos al pastor de la ciudad, con más de 20 años de tradición familiar.', 
+  'Calle Morelos #123, Centro', 
+  'comida', 
+  'https://tacoselguero.mx/wp-content/uploads/2021/01/img-20181130-233543433.jpg'
+);
+
+-- Negocio 2 (Comida)
+INSERT INTO negocios (nombre, descripcion, direccion, categoria, imagen)
+VALUES (
+  'Pizzería Napoli', 
+  'Auténtica pizza italiana horneada en horno de leña con ingredientes importados.', 
+  'Av. Hidalgo #456, Col. Juárez', 
+  'comida', 
+  'https://napolipizzasi.com/img/logo-w.png'
+);
+
+-- Negocio 3 (Servicios - si tienes esta categoría)
+INSERT INTO negocios (nombre, descripcion, direccion, categoria, imagen)
+VALUES (
+  'Lavandería Express', 
+  'Servicio rápido de lavado y planchado con entrega en 24 horas.', 
+  'Calle Reforma #789, Col. Centro', 
+  'servicios', 
+  'https://lavaexpressirenita.com.mx/wp-content/uploads/2020/09/logo-280x300.jpg'
+);
+
+-- Negocio 4 (Comida)
+INSERT INTO negocios (nombre, descripcion, direccion, categoria, imagen)
+VALUES (
+  'Café Literario', 
+  'Cafetería temática con libros para leer mientras disfrutas de nuestro café de especialidad.', 
+  'Av. Benito Juárez #1011, Zona Rosa', 
+  'comida', 
+  'http://sems2.sems.udg.mx/sites/default/files/files2013-16/banneercam/cartelcafeliterarioprepa3_2014.jpg'
+);
+
+-- Negocio 5 (Comida)
+INSERT INTO negocios (nombre, descripcion, direccion, categoria, imagen)
+VALUES (
+  'Sushi Factory', 
+  'Sushi fresco preparado al momento con pescado importado directamente de Japón.', 
+  'Plaza Galerías Local 25', 
+  'comida', 
+  'https://mid-townjalisco.com.mx/wp-content/uploads/2023/01/SUSHIFACTORYLOGO2.png'
+);
