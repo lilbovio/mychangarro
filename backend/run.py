5c7ae37775e81f4482d9e435bd93e5cb5b9d03f8
lilbovio <lilbovio30@gmail.com>
2025-05-09 04:27:25 -0600

    Configurando Flask para Render

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 from app import create_app
 
 app = create_app()
 CORS(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Usa el puerto de Render si está disponible
+    app.run(host="0.0.0.0", port=port, debug=False)
